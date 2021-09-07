713b2892ce4c5e300cff0829c7da5862ea769c8b
tanimoto <yumenikki2@gmail.com>
2021-09-07 21:08:40 +0900

    フロント側のお知らせデザイン作成完了。

--- laravel/resources/views/front/index.blade.php
+++ laravel/resources/views/front/index.blade.php
@@ -15,7 +15,79 @@
 <section id="pickUp">
   <h2 class="section-title">PICKUP</h2>
   <div class="pick-up-images">
+    <ul class="slider multiple-item">
+      <li><img src="{{ asset('assets/image/front/pickup1.jpg') }}" alt=""></li>
+      <li><img src="{{ asset('assets/image/front/pickup2.jpg') }}" alt=""></li>
+      <li><img src="{{ asset('assets/image/front/pickup3.jpg') }}" alt=""></li>
+      <li><img src="{{ asset('assets/image/front/pickup4.jpg') }}" alt=""></li>
+      <li><img src="{{ asset('assets/image/front/pickup5.jpg') }}" alt=""></li>
+      <li><img src="{{ asset('assets/image/front/pickup6.jpg') }}" alt=""></li>
+      <li><img src="{{ asset('assets/image/front/pickup7.jpg') }}" alt=""></li>
+      <li><img src="{{ asset('assets/image/front/pickup8.jpg') }}" alt=""></li>
+      <li><img src="{{ asset('assets/image/front/pickup9.jpg') }}" alt=""></li>
+    </ul>
+  </div>
+</section>
 
+<section id="notice">
+  <h2 class="section-title">NOTICE</h2>
+  <div class="notice-articles">
+    <div class="item">
+      <img src="{{ asset('assets/image/front/feature1.jpg') }}" alt="">
+      <div class="container">
+        <div class="title">category</div>
+        <p class="text">テキストテキストテキストテキスト</p>
+        <div class="date-time">日付</div>
+      </div>
+    </div>
+    <div class="item">
+      <img src="{{ asset('assets/image/front/feature2.jpg') }}" alt="">
+      <div class="container">
+        <div class="title">category</div>
+        <p class="text">テキストテキストテキストテキスト</p>
+        <div class="date-time">日付</div>
+      </div>
+    </div>
+    <div class="item">
+      <img src="{{ asset('assets/image/front/feature2.jpg') }}" alt="">
+      <div class="container">
+        <div class="title">category</div>
+        <p class="text">テキストテキストテキストテキスト</p>
+        <div class="date-time">日付</div>
+      </div>
+    </div>
+    <div class="item">
+      <img src="{{ asset('assets/image/front/feature2.jpg') }}" alt="">
+      <div class="container">
+        <div class="title">category</div>
+        <p class="text">テキストテキストテキストテキスト</p>
+        <div class="date-time">日付</div>
+      </div>
+    </div>
+    <div class="item">
+      <img src="{{ asset('assets/image/front/feature2.jpg') }}" alt="">
+      <div class="container">
+        <div class="title">category</div>
+        <p class="text">テキストテキストテキストテキスト</p>
+        <div class="date-time">日付</div>
+      </div>
+    </div>
+    <div class="item">
+      <img src="{{ asset('assets/image/front/feature2.jpg') }}" alt="">
+      <div class="container">
+        <div class="title">category</div>
+        <p class="text">テキストテキストテキストテキスト</p>
+        <div class="date-time">日付</div>
+      </div>
+    </div>
+    <div class="item">
+      <img src="{{ asset('assets/image/front/feature2.jpg') }}" alt="">
+      <div class="container">
+        <div class="title">category</div>
+        <p class="text">テキストテキストテキストテキスト</p>
+        <div class="date-time">日付</div>
+      </div>
+    </div>
   </div>
 </section>
 @endsection
